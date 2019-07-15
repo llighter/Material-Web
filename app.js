769bdb005120c5d89b914e794483752c1ca1d7c5
llighter <hadbsgk@gmail.com>
2019-07-16 01:07:21 +0900

    레이아웃: 블로그 처럼 타이틀 및 배열 정리

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 import { MDCRipple } from '@material/ripple/index';
 import { MDCTopAppBar } from '@material/top-app-bar';
+import { MDCTabBar } from '@material/tab-bar';
 
 
-// Instantiation
+// // Instantiation
 
-// ripple
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
+// // ripple
+// const ripple = new MDCRipple(document.querySelector('.foo-button'));
 
 // top-app-bar
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 
+// // tap bar
+const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
+
 // card
 const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
-const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
-  return new MDCRipple(el);
-});
+// const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
+//   return new MDCRipple(el);
+// });
+
+
+// tabBar.listen('MDCTabBar:activated', (activatedEvent) => {
+//   document.querySelectorAll('.adopt-a-pup-body').forEach((element, index) => {
+//     if (index === activatedEvent.detail.index) {
+//       element.classList.remove('adopt-a-pup-body--hidden');
+//     } else {
+//       element.classList.add('adopt-a-pup-body--hidden');
+//     }
+//   });
+// });
+
